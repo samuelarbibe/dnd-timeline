d39325b1adbc86d011f2279b891b980a4da1b935
Samuel Arbibe <samuel.arbibe@gmail.com>
2023-12-09 01:38:19 +0200

    Dedupe react in examples when running linked to local package

--- examples/virtual/vite.config.ts
+++ examples/virtual/vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  optimizeDeps: {
+    exclude: ["dnd-timeline"],
+  },
+  resolve: {
+    alias: {
+      react: path.resolve("./node_modules/react"),
+    },
+  },
+});
