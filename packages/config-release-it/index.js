3248f3dff823e0fcde7eb7f0e4b4ec6d338610c7
Samuel Arbibe <samuel.arbibe@gmail.com>
2023-12-17 21:06:45 +0200

    chore: add release-it config

---
+++ packages/config-release-it/index.js
@@ -0,0 +1,33 @@
+const version = '${version}';
+const packageName = process.env.npm_package_name;
+const scope = packageName.split('/')[1];
+
+module.exports = {
+  plugins: {
+    '@release-it/conventional-changelog': {
+      path: '.',
+      infile: 'CHANGELOG.md',
+      preset: 'conventionalcommits',
+      gitRawCommitsOpts: {
+        path: '.',
+      },
+    },
+  },
+  git: {
+    push: true,
+    tagName: `${packageName}@${version}`,
+    pushRepo: 'git@github.com:samuelarbibe/dnd-timeline.git',
+    commitsPath: '.',
+    commitMessage: `feat(${scope}): released version v${version} [no ci]`,
+    requireCommits: true,
+    requireCommitsFail: false,
+  },
+  npm: {
+    publish: true,
+    versionArgs: ['--workspaces false'],
+  },
+  github: {
+    release: true,
+    releaseName: `${packageName}-v${version}`,
+  }
+};
---
+++ packages/dnd-timeline/.release-it.js
@@ -0,0 +1,1 @@
+module.exports = require('@repo/release-it-config')
