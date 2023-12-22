58abd9126fa1dfbb964f92c79d38f4d9b2c55178
Samuel Arbibe <samuel.arbibe@gmail.com>
2023-12-22 18:09:21 +0200

    chore: remove redundant config from release-it

--- packages/config-release-it/index.js
+++ packages/config-release-it/index.js
@@ -10,17 +10,13 @@ module.exports = {
     '@release-it/conventional-changelog': {
       path: '.',
       infile: 'CHANGELOG.md',
-      preset: 'conventionalcommits',
-      gitRawCommitsOpts: {
-        path: '.',
-      },
+      preset: 'conventionalcommits'
     },
   },
   git: {
     push: true,
     tagName: `${packageName}@${version}`,
     pushRepo: 'git@github.com:samuelarbibe/dnd-timeline.git',
-    commitsPath: '.',
     commitMessage: `feat: released version v${version} [no ci]`,
     requireCommits: true,
     requireCommitsFail: false,
