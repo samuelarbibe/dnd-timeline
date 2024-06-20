17e2c63f69da1b49ec4286e6b35f89c1abf27ce9
Samuel Arbibe <samuel.arbibe@gmail.com>
2024-06-20 14:09:48 +0300

    chore: use pnpm with release-it

--- packages/config-release-it/index.js
+++ packages/config-release-it/index.js
@@ -1,33 +1,33 @@
-const version = '${version}';
+const version = "${version}";
 const packageName = process.env.npm_package_name;
-const scope = packageName.split('/')[1];
+const scope = packageName.split("/")[1];
 
 module.exports = {
-  hooks: {
-    "before:git:release": ["npm install", "git add --all"]
-  },
-  plugins: {
-    '@release-it/conventional-changelog': {
-      path: '.',
-      infile: 'CHANGELOG.md',
-      preset: 'conventionalcommits',
-    },
-  },
-  git: {
-    push: true,
-    tagName: `${packageName}@${version}`,
-    pushRepo: 'git@github.com:samuelarbibe/dnd-timeline.git',
-    commitMessage: `feat: released version v${version} [no ci]`,
-    requireCommits: true,
-    requireCommitsFail: false,
-    requireCleanWorkingDir: true
-  },
-  npm: {
-    publish: true,
-    versionArgs: ['--workspaces false'],
-  },
-  github: {
-    release: true,
-    releaseName: `${packageName}-v${version}`,
-  }
+	hooks: {
+		"before:git:release": ["pnpm install", "git add --all"],
+	},
+	plugins: {
+		"@release-it/conventional-changelog": {
+			path: ".",
+			infile: "CHANGELOG.md",
+			preset: "conventionalcommits",
+		},
+	},
+	git: {
+		push: true,
+		tagName: `${packageName}@${version}`,
+		pushRepo: "git@github.com:samuelarbibe/dnd-timeline.git",
+		commitMessage: `feat: released version v${version} [no ci]`,
+		requireCommits: true,
+		requireCommitsFail: false,
+		requireCleanWorkingDir: true,
+	},
+	npm: {
+		publish: true,
+		versionArgs: ["--workspaces false"],
+	},
+	github: {
+		release: true,
+		releaseName: `${packageName}-v${version}`,
+	},
 };
