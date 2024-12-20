9f6d244b4f4974e394e013e9882bf493397308fe
samuelarbibe <samuel.arbibe@gmail.com>
2024-12-20 16:33:19 +0200

    docs: add drag to create example

---
+++ examples/create/src/Row.tsx
@@ -0,0 +1,55 @@
+import type { RowDefinition, Span } from "dnd-timeline";
+import { useRow } from "dnd-timeline";
+import type React from "react";
+import { type PointerEvent, useCallback, useState } from "react";
+import CreateItem from "./CreateItem";
+
+interface RowProps extends RowDefinition {
+	children: React.ReactNode;
+	sidebar: React.ReactNode;
+	onCreateItem: (span: Span, rowId: string) => void;
+}
+
+function Row(props: RowProps) {
+	const {
+		setNodeRef,
+		setSidebarRef,
+		rowWrapperStyle,
+		rowStyle,
+		rowSidebarStyle,
+	} = useRow({ id: props.id });
+
+	const [dragStartX, setDragStartX] = useState<number | null>(null);
+
+	const handleOnPointerDown = (event: PointerEvent) => {
+		setDragStartX(event.clientX);
+	};
+
+	const handleOnCreateEnd = useCallback(
+		(span: Span) => {
+			props.onCreateItem(span, props.id);
+			setDragStartX(null);
+		},
+		[props.onCreateItem, props.id],
+	);
+
+	return (
+		<div style={{ ...rowWrapperStyle, minHeight: 50 }}>
+			<div ref={setSidebarRef} style={rowSidebarStyle}>
+				{props.sidebar}
+			</div>
+			<div
+				ref={setNodeRef}
+				style={{ ...rowStyle, border: "1px solid grey" }}
+				onPointerDown={handleOnPointerDown}
+			>
+				{props.children}
+				{dragStartX && (
+					<CreateItem startX={dragStartX} onCreateEnd={handleOnCreateEnd} />
+				)}
+			</div>
+		</div>
+	);
+}
+
+export default Row;
---
+++ examples/create/src/Timeline.tsx
@@ -0,0 +1,47 @@
+import type { ItemDefinition, RowDefinition, Span } from "dnd-timeline";
+import { groupItemsToSubrows, useTimelineContext } from "dnd-timeline";
+import { useMemo } from "react";
+import Item from "./Item";
+import Row from "./Row";
+import Sidebar from "./Sidebar";
+import Subrow from "./Subrow";
+
+interface TimelineProps {
+	rows: RowDefinition[];
+	items: ItemDefinition[];
+	onCreateItem: (span: Span, rowId: string) => void;
+}
+
+function Timeline(props: TimelineProps) {
+	const { setTimelineRef, style, range } = useTimelineContext();
+
+	const groupedSubrows = useMemo(
+		() => groupItemsToSubrows(props.items, range),
+		[props.items, range],
+	);
+
+	return (
+		<div ref={setTimelineRef} style={style}>
+			{props.rows.map((row) => (
+				<Row
+					id={row.id}
+					key={row.id}
+					sidebar={<Sidebar row={row} />}
+					onCreateItem={props.onCreateItem}
+				>
+					{groupedSubrows[row.id]?.map((subrow, index) => (
+						<Subrow key={`${row.id}-${index}`}>
+							{subrow.map((item) => (
+								<Item id={item.id} key={item.id} span={item.span}>
+									{`Item ${item.id}`}
+								</Item>
+							))}
+						</Subrow>
+					))}
+				</Row>
+			))}
+		</div>
+	);
+}
+
+export default Timeline;
